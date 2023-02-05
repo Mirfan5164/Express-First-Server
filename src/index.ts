bdf44e29f323dde4b61da0f368a11f90cb4af7db
Muhammad <mirfan00730@gmail.com>
2023-02-05 02:01:13 +0100

    Added HTTP Methods

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 //step#01: IMPORT EXPRESS REQUEST RESPONSE
 import Express, {request, Request, response, Response} from "express";
+import userData from "./userList/UserList";
 
 //step#02: EXPRESS FUNTION
 const app = Express();
@@ -8,8 +9,39 @@ const app = Express();
 app.use(Express.json());
 
 //step#04: FRONTEND-BACKEND | REQUEST & RESPONSE
-app.get("/", (req: Request, res: Response) => {
-    res.json("This is from get") && res.status(201);
+
+//(a): GET METHOD
+app.get("/users", (req: Request, res: Response) => {
+    res.json(userData) && res.status(200); 
+});
+
+//(b): POST METHOD
+app.post("/users", (req: Request, res: Response) => {
+    const newUser = req.body;
+    userData.push(newUser);
+    res.json(userData) && res.status(201); 
+});
+
+//(c): DELETE METHOD
+app.delete("/users/:id", (req: Request, res: Response) => {
+    const result = userData.filter(user => user.id !== Number(req.params.id));
+    res.json(result) && res.status(200); 
+});
+
+//(d): PUT METHOD
+app.put("/users/:id", (req: Request, res: Response) => {
+    const updateUser = req.body;
+    const objIndex = userData.findIndex(user => user.id === Number(req.params.id));
+    userData[objIndex] = updateUser
+    res.json(userData) && res.status(200); 
+});
+
+//(e): PATCH METHOD
+app.patch("/users/:id", (req: Request, res: Response) => {
+    const updateUser = req.body;
+    const objIndex = userData.findIndex(user => user.id === Number(req.params.id));
+    userData[objIndex].name = updateUser.name;
+    res.json(userData) && res.status(200); 
 });
 
 //step#05: LISTEN FUNCTION
---
+++ src/userList/UserList.ts
@@ -0,0 +1,9 @@
+let userData = [
+    {id: 1, name: "Ali", age: 10},
+    {id: 2, name: "Salim", age: 12},
+    {id: 3, name: "Arsalan", age: 15},
+    {id: 4, name: "Sameer", age: 11},
+    {id: 5, name: "Kashif", age: 14},
+]
+
+export default userData;
